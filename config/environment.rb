707cdf8e9b5e9371e2fabc11f3cf9c4be645d30f
root <root@DESKTOP-0OBGIJ0.localdomain>
2019-04-23 10:26:50 -0400

    ActiveRecord and Carrierwave

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require "carrierwave/orm/activerecord"
 Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
-
+#
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
